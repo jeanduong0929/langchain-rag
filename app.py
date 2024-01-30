f33375babf17462a29617e9c7fedcf79539e9c2a
bduong <bduong@accuregsoftware.com>
2024-01-30 15:47:32 -0500

    Add streamlit chat interface and session state management

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 from chroma_client import ChromaClient
 from langchain_openai import ChatOpenAI
 from langchain.prompts import MessagesPlaceholder, ChatPromptTemplate
@@ -6,20 +8,21 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.messages import HumanMessage, AIMessage
 
 
-def get_user_input():
-    return input(
-        "Hello I am a powerlifting chatbot. Ask me a question (exit to quit): "
-    ).lower()
-
-
 def main():
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
     # Intialize the vector store
-    chroma_db = ChromaClient()
+    st.session_state.chroma_db = ChromaClient()
 
     # Initialize OpenAI LLM
-    openai_llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
+    st.session_state.openai_llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.0)
 
-    prompt = ChatPromptTemplate.from_messages(
+    # Create the prompt for the chatbot
+    st.session_state.prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
@@ -29,10 +32,10 @@ def main():
                 3. In cases where the context does not contain the information needed to answer a query, clearly state that the answer is not available in the provided documents.
                 4. Do not make assumptions or create answers based on general knowledge. Stick strictly to the content of the context.
                 5. Maintain a professional tone, befitting a powerlifting coach, focusing on providing accurate and reliable information to assist in training and competition preparation.
-                6. Quote the source from the context metadata
-    
+                6. Quote the source from the context metadata. The source should contain the name of the author, the title of the document, and page number.
+
                 Your primary role is to assist, inform, and guide individuals interested in powerlifting by utilizing the specific information provided in the context documents.
-    
+
                 ---
                 Context: {context}
                 ---
@@ -43,39 +46,47 @@ def main():
         ]
     )
 
-    document = create_stuff_documents_chain(llm=openai_llm, prompt=prompt)
-
-    conv_retrieval_chain = create_retrieval_chain(
-        retriever=chroma_db.vector_store.as_retriever(search_kwargs={"k": 3}),
-        combine_docs_chain=document,
+    st.session_state.document = create_stuff_documents_chain(
+        llm=st.session_state.openai_llm, prompt=st.session_state.prompt
     )
 
-    chat_history = []
-    while True:
-        print("\n============================\n")
+    st.session_state.conversation = create_retrieval_chain(
+        retriever=st.session_state.chroma_db.vector_store.as_retriever(
+            search_kwargs={"k": 3}
+        ),
+        combine_docs_chain=st.session_state.document,
+    )
 
-        # Get user input
-        query = get_user_input()
+    # Setting up the page title and icon
+    st.set_page_config(page_title="Powerlifting Chatbot", page_icon=":weight_lifter:")
 
-        # Quit if user types exit
-        if query == "exit":
-            break
+    # Setting up the header
+    st.header("Powerlifting Chatbot :weight_lifter:")
 
+    if user_question := st.chat_input("Ask me a question"):
         # Query the user input
-        result = conv_retrieval_chain.invoke(
-            {
-                "chat_history": chat_history,
-                "input": query,
-            }
-        )
+        with st.spinner("Thinking..."):
+            result = st.session_state.conversation.invoke(
+                {"chat_history": st.session_state.chat_history, "input": user_question}
+            )
+
+        # Save the chat history to the session state
+        st.session_state.chat_history = result["chat_history"]
 
         # Add the user input and response to the chat history
-        chat_history.extend(
-            [HumanMessage(content=query), AIMessage(content=result["answer"])]
+        st.session_state.chat_history.extend(
+            [HumanMessage(content=user_question), AIMessage(content=result["answer"])]
         )
 
-        print("\n")
-        print(result["answer"])
+        # Iterate through the chat history and display the messages
+        st.session_state.chat_history = result["chat_history"]
+        for i, message in enumerate(result["chat_history"]):
+            if i % 2 == 0:
+                with st.chat_message("user"):
+                    st.write(message.content)
+            else:
+                with st.chat_message("ai"):
+                    st.write(message.content)
 
 
 if __name__ == "__main__":
